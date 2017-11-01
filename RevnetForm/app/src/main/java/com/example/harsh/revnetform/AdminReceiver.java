b2a680b0e6dd3e90b0022d979828017b38366584
Harsh <shahh94@gmail.com>
2017-11-01 11:33:15 -0400

    final commit

---
+++ RevnetForm/app/src/main/java/com/example/harsh/revnetform/AdminReceiver.java
@@ -0,0 +1,12 @@
+package com.example.harsh.revnetform;
+
+import android.app.admin.DeviceAdminReceiver;
+
+/**
+ * Created by Harsh on 6/19/17.
+ */
+
+public class AdminReceiver extends DeviceAdminReceiver {
+
+
+}
